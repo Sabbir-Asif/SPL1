caa93339308e93b50e1d8d7c2e719cd42670f51c
Sabbir_Asif <113287134+Sabbir-Asif@users.noreply.github.com>
2023-05-10 21:46:59 +0600

    Add files via upload

--- NDVI.cpp
+++ NDVI.cpp
@@ -4,50 +4,50 @@
 using namespace std;
 
 
-void CalculateNDVI(BMPImage* bmp1,const char* ipfile)
+void CalculateNDVI(BMPImage* bmp1, BMPImage* bmp2, BMPImage* bmp3, const char* ipfile)
 {
-    BMPImage* bmp2 = new BMPImage;
     float ndvi,numerator,denominator;
-    if(readBmpImage(bmp2,ipfile))
+    if(readBmpImage(bmp3,ipfile))
     {
         for(int i = 0; i < bmp1->infoHeader.height; i++)
         {
             for(int j  = 0; j < bmp1->infoHeader.width; j++)
             {
-                numerator = static_cast<float>(bmp1->pixel[i][j][1] - bmp1->pixel[i][j][2]);
-                denominator = static_cast<float>(bmp1->pixel[i][j][1] + bmp1->pixel[i][j][2]);
-                if(denominator == 0.0)
+                numerator = bmp2->pixel[i][j][2] - bmp1->pixel[i][j][0];
+                denominator = bmp2->pixel[i][j][2] + bmp1->pixel[i][j][0];
+                if(denominator == 0)
                 {
                     denominator = 1.0;
                 }
                 else
                 {
-                    ndvi = static_cast<float>(numerator/denominator);
-                    //bmp2->pixel[i][j][0] = static_cast<unsigned char>(200 - ((ndvi*100)+100));
-                    bmp2->pixel[i][j][1] = static_cast<unsigned char>((ndvi*100)+150);
-                    //bmp3->pixel[i][j][2] = static_cast<unsigned char>(200 - ((ndvi*100)+100));
+                    ndvi = numerator/denominator;
+                    
                 }
             }
         }
     }
-    const char* opfile;
-    opfile = "outNDVI.bmp";
-    constructBmpImage(bmp2,opfile);
 }
 int main()
 {
 	
 	BMPImage* bmp1 = new BMPImage;
-	const char* ipfile;
+    BMPImage* bmp2 = new BMPImage;
+    BMPImage* bmp3 = new BMPImage;
+	const char* ipfile1;
+    const char* ipfile2;
+	const char* opfile;
 
-	ipfile = "dhaka.bmp";
-	
-	printf("Input file : %s\n", ipfile);
+	ipfile1 = "b4.bmp";
+	ipfile2 = "b5.bmp";
+	opfile = "outNDVI.bmp";
+	printf("Input file : %s %s\n", ipfile1, ipfile2);
  
 	// Process the image and print its details
-	if (readBmpImage(bmp1, ipfile))
+	if (readBmpImage(bmp1, ipfile1) && readBmpImage(bmp2,ipfile2))
 	{
-        CalculateNDVI(bmp1,ipfile);
+        CalculateNDVI(bmp1,bmp2,bmp3,ipfile1);
+        constructBmpImage(bmp3,opfile);
 	}
 	return 0;
 }
